2db053b98d57acc3de7b8e527dfff69e9f0e0ec3
gidyon <gideon.ngk@gmail.com>
2024-01-16 11:39:11 +0300

    removed unused logs

--- internal/imgcompress/compress.go
+++ internal/imgcompress/compress.go
@@ -15,15 +15,11 @@ func CompressImage(c *fiber.Ctx) error {
 		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"message": fmt.Sprintf("failed to get image from request: %v", err)})
 	}
 
-	fmt.Println("Gotten image header")
-
 	// Open the file
 	file, err := fileHeader.Open()
 	if err != nil {
 		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"message": fmt.Sprintf("failed to open file: %v", err)})
 	}
-
-	fmt.Println("Gotten image")
 
 	// Compress image
 	img, err := imaging.Decode(file)
@@ -31,12 +27,8 @@ func CompressImage(c *fiber.Ctx) error {
 		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"message": fmt.Sprintf("failed to get image from request: %v", err)})
 	}
 
-	fmt.Println("Image decode done")
-
 	// Resize the cropped image to width = 200px preserving the aspect ratio.
 	src := imaging.Resize(img, 500, 0, imaging.Lanczos)
-
-	fmt.Println("Compression done")
 
 	c.Set(fiber.HeaderContentType, "image/png")
 
@@ -45,8 +37,6 @@ func CompressImage(c *fiber.Ctx) error {
 	if err != nil {
 		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"message": fmt.Sprintf("failed to get image from request: %v", err)})
 	}
-
-	fmt.Println("sending done")
 
 	// Send back compressed image
 	return nil
