91bcfdce104a074deca43c7a28ae5da3ff366f74
gidyon <gideon.ngk@gmail.com>
2025-04-22 08:35:37 +0300

    added h and w

--- internal/img/compress.go
+++ internal/img/compress.go
@@ -3,6 +3,7 @@ package img
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/disintegration/imaging"
 	"github.com/gofiber/fiber/v2"
@@ -13,6 +14,19 @@ func CompressImage(c *fiber.Ctx) error {
 	fileHeader, err := c.FormFile("image")
 	if err != nil {
 		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"message": fmt.Sprintf("failed to get image from request: %v", err)})
+	}
+
+	width := c.FormValue("width")
+	height := c.FormValue("height")
+
+	w, err := strconv.Atoi(width)
+	if err != nil {
+		w = Width
+	}
+
+	h, err := strconv.Atoi(height)
+	if err != nil {
+		h = 0
 	}
 
 	// Open the file
@@ -28,12 +42,12 @@ func CompressImage(c *fiber.Ctx) error {
 	}
 
 	// Resize the cropped image to  preserving the aspect ratio.
-	src := imaging.Resize(img, Width, 0, imaging.Lanczos)
+	src := imaging.Resize(img, w, h, imaging.Lanczos)
 
 	c.Set(fiber.HeaderContentType, "image/png")
 
 	// Send image back to browser
-	err = imaging.Encode(c, src, imaging.PNG)
+	err = imaging.Encode(c, src, imaging.JPEG)
 	if err != nil {
 		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"message": fmt.Sprintf("failed to emcode compressed image: %v", err)})
 	}
